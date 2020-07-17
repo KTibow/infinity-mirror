2038e7fa1d3990729e267db34c1e0b22ba7550cc
Kendell R <KTibow@users.noreply.github.com>
2020-07-17 13:47:19 -0700

    Create token.h

---
+++ mirror/token.h
@@ -0,0 +1,4 @@
+String HA_URL = "myhomeassistant.duckdns.org";
+String HA_TOKEN = "generate_this_by_going_to_your_profile_page_in_home_assistant_and_clicking_generate_personal_access_token";
+String WIFI_NAME = "MyWifiNetwork!";
+String WIFI_PASS = "myVEEEEEEEEEEry_SeCuRe+PASSW0RD";
