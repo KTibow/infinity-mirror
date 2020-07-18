844ce506324c4ba26c2995d68db76dc64f2fdcd4
Kendell R <KTibow@users.noreply.github.com>
2020-07-18 10:45:04 -0700

    Update otaroute.h

--- mirror/otaroute.h
+++ mirror/otaroute.h
@@ -6,6 +6,24 @@ server.on("/serverIndex", HTTP_GET, []() {
 server.sendHeader("Connection", "close");
 server.send(200, "text/html", serverIndex);
 });
+server.on("/collect", HTTP_GET, []() {
+int paramsNr = server.args();
+String message = "Recieved. Param number: ";
+message += paramsNr;
+if (paramsNr >= 1) {
+message += " First arg name: ";
+message += server.argName(0);
+if (server.argName(0) == "info") {
+Serial.print("Recieved data: ");
+Serial.println(server.arg(0));
+updateHA(server.arg(0));
+message += " Recieved data: ";
+message += server.arg(0);
+}
+}
+server.sendHeader("Connection", "close");
+server.send(200, "text/plain", message);
+});
 server.on("/update", HTTP_POST, []() {
 server.sendHeader("Connection", "close");
 server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
